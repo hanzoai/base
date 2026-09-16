6202e8dc3fb2db5e565e635a632dbf95e57f6863
Zach Kelling <z@zeekay.io>
2026-09-15 20:49:00 -0700

    working tree of /home/z/base-chk on spark at retirement

--- plugins/extbench/fixtures/wazero-rust/target/wasm32-unknown-unknown/release/validate_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/z/work/hanzo/base/plugins/extbench/fixtures/wazero-rust/target/wasm32-unknown-unknown/release/validate_rust.wasm: /Users/z/work/hanzo/base/plugins/extbench/fixtures/wazero-rust/src/lib.rs
